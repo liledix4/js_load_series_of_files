5cdd0c9bcc1311e489cd4d3c31316c7a6a4785be
liledix4 <liledix4@musician.org>
2025-02-07 22:17:58 +0000

    0.1.0

--- load_series_of_files.js
+++ load_series_of_files.js
@@ -1,103 +1,107 @@
 import { readTextFile } from '../js_xhr_ajax/xhr_ajax.js';
 
 
-export let fileData = [];
+let fileData = [];
 
 
-export function loadSeriesOfFiles(startingFilePath, callback) {
-    readTextFile(
-        {
-            url: startingFilePath
-        },
-        rawFileContent => {
-            const obj = JSON.parse(rawFileContent);
-            const fileDirectory = startingFilePath.match(/.*\//)[0];
-            let addToFileData = null;
+export function loadSeriesOfFiles(startingFilePath, objectKeyForArrayWithFilePaths, callback) {
+  readTextFile(
+    {
+      url: startingFilePath
+    },
+    rawFileContent => {
+      let obj = JSON.parse(rawFileContent);
+      const fileDirectory = startingFilePath.match(/.*\//)[0];
+      let addToFileData = null;
 
-            if (obj.list_of_features)
-                addToFileData = {
-                    file: startingFilePath,
-                    list_of_features: obj.list_of_features,
-                    subfiles: null
-                };
-            if (obj.load_json_files)
-                obj.load_json_files.forEach(nextFilePath => {
-                    const nextFilePathFull = fileDirectory + nextFilePath;
+      addToFileData = {
+        file: startingFilePath,
+        subfiles: null,
+        content: null
+      };
 
-                    if (addToFileData !== null) {
-                        if (addToFileData.subfiles === null)
-                            addToFileData.subfiles = [];
-                        addToFileData.subfiles.push(nextFilePathFull);
-                    }
+      if (obj[objectKeyForArrayWithFilePaths]) {
+        obj[objectKeyForArrayWithFilePaths].forEach(nextFilePath => {
+          const nextFilePathFull = fileDirectory + nextFilePath;
 
-                    loadSeriesOfFiles(nextFilePathFull);
-                });
+          if (addToFileData !== null) {
+            if (addToFileData.subfiles === null)
+              addToFileData.subfiles = [];
+            addToFileData.subfiles.push(nextFilePathFull);
+          }
 
-            if (addToFileData !== null)
-                fileData.push(addToFileData);
-            if (callback)
-                checkIfFilesAreLoaded(callback);
-        }
-    );
+          loadSeriesOfFiles(nextFilePathFull, objectKeyForArrayWithFilePaths);
+        });
+        delete obj[objectKeyForArrayWithFilePaths];
+      }
+      if (Object.keys(obj).length > 0)
+        addToFileData.content = obj;
+
+      if (addToFileData !== null)
+        fileData.push(addToFileData);
+      if (callback)
+        return checkIfFilesAreLoaded(callback);
+    }
+  );
 }
 
 
 export function checkIfFilesAreLoaded(callback) {
-    let listOfFileNames = [];
-    let testPassed = false;
+  let listOfFileNames = [];
+  let testPassed = false;
 
-    function addToList(path) {
-        if (listOfFileNames.filter(a => a === path).length === 0)
-            listOfFileNames.push(path);
+  function addToList(path) {
+    if (listOfFileNames.filter(a => a === path).length === 0)
+      listOfFileNames.push(path);
+  }
+  function testFailed() {
+    const fileCount = listOfFileNames.length;
+    console.error('⚠️ Test failed');
+    console.error(`%d files detected so far. Retrying in 2 seconds...`, fileCount);
+    setTimeout(() => { checkIfFilesAreLoaded(callback) }, 2000);
+  }
+  function testComplete() {
+    const fileCount = listOfFileNames.length;
+    console.info('✅ Test complete');
+    console.info(`%d files were detected`, fileCount);
+    callback(fileData);
+  }
+
+  // Step 0
+
+  if (fileData.length === 0) {
+    testFailed();
+    return;
+  }
+
+  // Step 1
+
+  fileData.forEach(obj => {
+    addToList(obj.file);
+    if (obj.subfiles !== null)
+      obj.subfiles.forEach(subfilePath => {
+        addToList(subfilePath);
+      });
+  });
+
+  // Step 2
+
+  testPassed = listOfFileNames.every(file => {
+    const filteredObject = fileData.filter(a => a.file === file);
+    if (filteredObject.length === 0)
+      return false;
+    else if (filteredObject.length === 1 && filteredObject[0].content !== null)
+      return true;
+    else {
+      console.error('Something is wrong: ambiguous files found.');
+      return false;
     }
-    function testFailed() {
-        const fileCount = listOfFileNames.length;
-        console.error('⚠️ Test failed');
-        console.error(`%d files detected so far. Retrying in 2 seconds...`, fileCount);
-        setTimeout(() => { checkIfFilesAreLoaded(callback) }, 2000);
-    }
-    function testComplete() {
-        const fileCount = listOfFileNames.length;
-        console.info('✅ Test complete');
-        console.info(`%d files were detected`, fileCount);
-        callback();
-    }
+  });
 
-    // Step 0
+  // Step 3
 
-    if (fileData.length === 0) {
-        testFailed();
-        return;
-    }
-
-    // Step 1
-
-    fileData.forEach(obj => {
-        addToList(obj.file);
-        if (obj.subfiles !== null)
-            obj.subfiles.forEach(subfilePath => {
-                addToList(subfilePath);
-            });
-    });
-
-    // Step 2
-
-    testPassed = listOfFileNames.every(file => {
-        const filteredObject = fileData.filter(a => a.file === file);
-        if (filteredObject.length === 0)
-            return false;
-        else if (filteredObject.length === 1 && filteredObject[0].list_of_features.length > 0)
-            return true;
-        else {
-            console.error('Something is wrong: ambiguous files found.');
-            return false;
-        }
-    });
-
-    // Step 3
-
-    if (testPassed === true)
-        testComplete();
-    else if (testPassed === false)
-        testFailed();
+  if (testPassed === true)
+    testComplete();
+  else if (testPassed === false)
+    testFailed();
 }
